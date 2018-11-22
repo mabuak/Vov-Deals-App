adf4af947dedc5a39d9fbbc2f7176921571f137b
Fachruzi Ramadhan <mabuak@live.com>
2018-11-22 10:10:05 +0700

    Initial Commit

---
+++ Vov Deals App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vov Deals App
+//
+//  Created by Fachruzi Ramadhan on 22/11/2018.
+//  Copyright © 2018 Fachruzi Ramadhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
